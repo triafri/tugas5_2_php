c4ca62081be141224f0a1a2aa2406974cb5d5ac3
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-10-08 10:27:44 +0700

    Add files via upload

---
+++ tugas5_2.php
@@ -0,0 +1,23 @@
+<?php 
+$input1 = ["Ayo", "Belajar"];
+$input2 = ["Bersama", "Niomic"];
+
+echo "<br>";
+echo "<table width = 500 border =1 >
+		<tr align=center>
+			<td>Input 1</td>
+			<td>Input 2</td>
+			<td>Hasil</td>
+		</tr>
+		<tr align=center>
+			<td>".$input1[0]."</td>
+			<td>".$input2[0]."</td>
+			<td>".$input1[0]." ".$input2[0]."</td>
+		</tr>
+		<tr align=center>
+			<td>".$input1[1]."</td>
+			<td>".$input2[1]."</td>
+			<td>".$input1[1]." ".$input2[1]."</td>
+		</tr>
+		<table>";
+?>
